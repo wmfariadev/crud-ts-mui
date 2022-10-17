84c222cfa1901e5e25b8e7970e04b09cc2d3cc9f
Willian de Moraes Faria <willian.faria@mult-e.com.br>
2022-10-17 15:47:58 -0300

    feat: adicionado react-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Olá mundo</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
