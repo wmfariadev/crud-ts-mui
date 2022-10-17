d635e0cdc5dc1df71b491ad7e6ea1a221c630e88
Willian de Moraes Faria <willian.faria@mult-e.com.br>
2022-10-17 16:22:12 -0300

    feat: inclusão de lib MUI v5 e theme customizado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Button } from "@mui/material"
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Página Inical</Button>} />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
